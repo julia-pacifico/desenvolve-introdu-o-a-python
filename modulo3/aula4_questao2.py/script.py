58ddc99548aac4ee639f27f4b7cc1d6756558908
Julia Pacifico <julia.barbosa@pditabira.com>
2024-06-20 20:27:00 -0300

    Add files via upload

---
+++ modulo3/aula4_questao2.py/script.py
@@ -0,0 +1,11 @@
+avaliação_dos_usuarios=int(input("insira uma avaliação de um filme em uma escala de 1 a 5:"))
+if (avaliação_dos_usuarios)== 5:
+    print("Excelente!")
+if (avaliação_dos_usuarios)== 4:
+    print("Muito bom!")
+if (avaliação_dos_usuarios)== 3:
+    print("Bom!")
+if (avaliação_dos_usuarios)== 2:
+    print("Regular!")
+if (avaliação_dos_usuarios)== 1:
+    print("Ruim!")
